f81a899f0fd87771168b60978e7bda477e867f9f
jackHotch <jhotchkiss2021@gmail.com>
2025-02-18 21:37:10 -0500

    removed protected routes

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,30 +1,16 @@
 'use client'
 
-import { auth } from '@/lib/firebase'
 import { useEffect, useState } from 'react'
-import { useRouter } from 'next/navigation'
 import { DataTable } from '@/components/data-table'
 import { ProfileIcon } from '@/components/profile-icon'
 import { getFlags } from '@/lib/featureflag'
 
 export default function Dashboard() {
-  const router = useRouter()
   const [flags, setFlags] = useState([])
-  const [isUserLoggedIn, setIsUserLoggedIn] = useState(false)
 
   useEffect(() => {
-    console.log(auth?.currentUser?.email)
-    // if (auth?.currentUser) {
-    //   setIsUserLoggedIn(true)
     getFlags(setFlags)
-    // } else {
-    //   router.push('/')
-    // }
   }, [])
-
-  // if (!isUserLoggedIn) {
-  //   return null
-  // }
 
   return (
     <div className='flex flex-col items-center p-6 md:p-10'>
